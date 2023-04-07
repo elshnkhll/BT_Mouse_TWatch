5541829934512427fbd2fdd5b35a13994ee02b5e
ELSHNKHLL <elshnkhll@yahoo.com>
2023-04-07 08:51:45 -0400

    Add files via upload

---
+++ config.h
@@ -0,0 +1,9 @@
+#define LILYGO_WATCH_2020_V1              // To use T-Watch2020 , please uncomment this line
+
+
+// NOT SUPPORT ...
+//// #define LILYGO_WATCH_2019_NO_TOUCH
+//// #define LILYGO_WATCH_BLOCK
+// NOT SUPPORT ...
+
+#include <LilyGoWatch.h>
